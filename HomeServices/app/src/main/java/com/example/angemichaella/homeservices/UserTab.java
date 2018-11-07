d08fcb8772314dfb0f592fce97571be09551e818
Ange Michaella <angemichaella@yahoo.fr>
2018-11-06 22:07:14 -0500

    made a few changes to the user tab

--- HomeServices/app/src/main/java/com/example/angemichaella/homeservices/UserTab.java
+++ HomeServices/app/src/main/java/com/example/angemichaella/homeservices/UserTab.java
@@ -8,20 +8,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link UserTab.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link UserTab#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class UserTab extends Fragment {
+
+    ListView users_list;
+
+    List<User> users;
+    DatabaseReference databaseUsers;
+
 
     public UserTab() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,

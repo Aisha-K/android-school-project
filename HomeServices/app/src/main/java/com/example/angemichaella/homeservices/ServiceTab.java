56377cb987b96f62be74f570ccce40b39061c95c
Aisha Khalid <akhal096@uottawa.ca>
2018-11-07 22:44:36 -0500

    Added database so the list updates. Also added functions to be implemented

--- HomeServices/app/src/main/java/com/example/angemichaella/homeservices/ServiceTab.java
+++ HomeServices/app/src/main/java/com/example/angemichaella/homeservices/ServiceTab.java
@@ -12,6 +12,12 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,6 +35,9 @@ public class ServiceTab extends Fragment {
 
     private Button addServiceBtn;
     private ArrayList<Service> services;
+    DatabaseReference databaseServices;
+    ListView serviceListView;
+    //View view;
 
 
     @Override
@@ -36,25 +45,38 @@ public class ServiceTab extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_service_tab, container, false);
         addServiceBtn = (Button)view.findViewById(R.id.addServiceBtn);
-
-    //Example Case For Now
-        Service s1 = new Service("Painting", false, 23.478, "a1");
-        Service s2 = new Service("Lawn Mowing", true, 50, "b2");
-        Service s3 = new Service("Furniture Moving", false, 12.34, "c3");
-
-        s1.iconName = "paint_icon";
-        s2.iconName = "grass_icon";
-        s3.iconName = "couch_icon";
-
         services = new ArrayList<Service>();
-        services.add(s1);
-        services.add(s2);
-        services.add(s3);
-    //end of example case
+        databaseServices = FirebaseDatabase.getInstance().getReference("Services");
+        serviceListView = (ListView)view.findViewById(R.id.serviceListView);
 
-        ServiceAdapter adtr = new ServiceAdapter(getActivity(), services);
-        ListView serviceListView = (ListView)view.findViewById(R.id.serviceListView);
-        serviceListView.setAdapter(adtr);
+
+        databaseServices.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                services.clear();
+
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
+                    Service currService = postSnapshot.getValue(Service.class); //retrieving child node
+                    //if(!currService.type().equals("indoor")) {
+                    services.add(currService);                          //adding service from database to list
+                    //}
+                }
+                ServiceAdapter adtr = new ServiceAdapter(getActivity(), services);
+                serviceListView.setAdapter(adtr);
+
+            }
+            public void onCancelled(DatabaseError databaseError){
+            }
+        });
+
+
+        //when new serivce button is clicked, will call function new Service
+        addServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newService();
+            }
+        });
 
 
         //example of when a service is clicked
@@ -63,12 +85,44 @@ public class ServiceTab extends Fragment {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
                         String srvName = String.valueOf(((Service)parent.getItemAtPosition(pos)).name());
+                        String srvId= String.valueOf(((Service)parent.getItemAtPosition(pos)).serviceId);
                         Toast.makeText(getActivity(), srvName, Toast.LENGTH_LONG).show();
                     }
                 }
         );
 
         return view;
+    }
+
+//    @Override
+//    public void onStart(){
+//        super.onStart();
+//
+//    }
+
+    //adds a service to the database if name not already taken
+    private void newService(){
+
+        //test case
+        Service s3= new Service("Plumbing", false, 30.0, "d4");
+        databaseServices.child(s3.serviceId).setValue(s3);
+    }
+
+    //shows pop up which allows user to either edit or delete the service that was clicked on
+    //this method calls the deleteService() if delete button clicked
+    // or  editService() if update button clicked with appropriate text entered
+    private void showEditOrDeletePopUp( String serviceName, String serviceId){
+
+    }
+
+    //deletes a service from the database
+    private void deleteService( String ServiceId){
+
+    }
+
+    //updates a service in the database with new info
+    private void editService(String ServiceId, String newName, double newPrice){
+
     }
 
 

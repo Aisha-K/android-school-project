866d5c930c4a1320f137ca039931ff09c4c2381a
Ange Michaella <angemichaella@yahoo.fr>
2018-11-15 00:22:53 -0500

    implemented getters/setters method for the ServiceProvider class

--- app/src/main/java/com/example/angemichaella/homeservices/ServiceProvider.java
+++ app/src/main/java/com/example/angemichaella/homeservices/ServiceProvider.java
@@ -2,6 +2,12 @@ package com.example.angemichaella.homeservices;
 
 public class ServiceProvider extends User {
 
+    private String address = "";
+    private String phoneNumber = "";
+    private String companyName = "";
+    private String description = "";
+    private boolean isLicensed = false;
+
     //default constructor
     public ServiceProvider(){
         super();
@@ -15,5 +21,44 @@ public class ServiceProvider extends User {
 
     public ServiceProvider(String userName, String password, String userId, String type){
         super(userName, password, userId, type);
+    }
+
+    // getters methods
+    public String getAddress(){
+        return address;
+    }
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+    public String getCompanyName(){
+        return companyName;
+    }
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean isLicensed() {
+        return isLicensed;
+    }
+
+    // setters methods
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setLicensed(boolean licensed) {
+        isLicensed = licensed;
     }
 }

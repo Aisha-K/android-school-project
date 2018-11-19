24fafd7710144be7a5170356125a3236a1d77feb
Yusra Ozohu <yadin030@uottawa.ca>
2018-11-18 21:28:36 -0500

    updated Time so it shall work with the database

--- app/src/main/java/com/example/angemichaella/homeservices/Time.java
+++ app/src/main/java/com/example/angemichaella/homeservices/Time.java
@@ -8,21 +8,21 @@ public class Time {
 
     private int hour;
     private int minute;
-    private int time; //in minutes after midnight
+    private int tense; //in minutes after midnight
 
-    public Time(int hr, int min, int tense){
+    public Time(int hr, int min, int tns){
         hour = hr;
         minute = min;  
         if(hour == 12){
             hour = 0;
         }
+        tense = tns;
         hour += 12*tense;
-        time = 60*hour + minute;
 
     }
 
     public int time(){
-        return time;
+        return 60*(hour+12*tense) + minute;
     }
 
     public String toString(){//RETURNS TIME IN 24 HR FORMAT
@@ -59,6 +59,15 @@ public class Time {
 
     }
 
+    public int getHour() {
+        return hour;
+    }
 
+    public int getMinute() {
+        return minute;
+    }
 
+    public int getTense() {
+        return tense;
+    }
 }

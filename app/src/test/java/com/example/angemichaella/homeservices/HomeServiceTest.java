3e62c0c785894f32626842962b78f7f82ea1e6cd
Ange Michaella <angemichaella@yahoo.fr>
2018-11-19 22:18:48 -0500

    added the new additional unit test cases

--- app/src/test/java/com/example/angemichaella/homeservices/HomeServiceTest.java
+++ app/src/test/java/com/example/angemichaella/homeservices/HomeServiceTest.java
@@ -31,9 +31,25 @@ public class HomeServiceTest {
 
     @Test
     public void checkUserName(){
-        User aUser = new User("name", "password", "myid");
+        User aUser = new User("name", "password", "myEmailAddress","myid", "myType");
         assertEquals("Check the name of the user", "name", aUser.getUsername());
     }
+
+
+    // 2 NEW UNIT TEST CASES ADDED & RELATED TO DELIVERABLE 3
+
+    @Test
+    public void checkAvailabilityDay(){
+        Availability av = new Availability(Day.MONDAY, new Time(1,0,1), new Time(1,10,1));
+        assertEquals("Check the day of the availability", Day.MONDAY,av.getDay());
+    }
+
+    @Test
+    public void checkAvailabilityTimeFrom(){
+        Availability av = new Availability(Day.MONDAY, new Time(1,0,1), new Time(1,10,1));
+        assertEquals("Check the time from of the availability", new Time(1,0,1),av.getFrom());
+    }
+
 }
 
 

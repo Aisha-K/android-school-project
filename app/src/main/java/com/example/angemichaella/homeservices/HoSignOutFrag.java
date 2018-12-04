aa2aaee5d407bd364395cb709bda227534026131
Aisha Khalid <akhal096@uottawa.ca>
2018-12-04 10:51:39 -0500

    added sign out functionality to HO

--- app/src/main/java/com/example/angemichaella/homeservices/HoSignOutFrag.java
+++ app/src/main/java/com/example/angemichaella/homeservices/HoSignOutFrag.java
@@ -1,10 +1,12 @@
 package com.example.angemichaella.homeservices;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HoSignOutFrag extends Fragment {
 
@@ -13,6 +15,23 @@ public class HoSignOutFrag extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_ho_signout, container, false);
+
+
+        Button SignOutBtn = view.findViewById(R.id.HoSignOutBtn);
+
+        //clicking the sign out button
+        SignOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //send to main activity and finish this one
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
         return view;
+
+
     }
 }
